c187d1c1c7d79755e99c988bee98158e9f753857
Mubeen Bankur <mobee33@hotmail.com>
2022-06-09 20:45:07 +0400

    added controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tender, only: [:index, :new, :create] do
+    resources :bid, only: [:index, :new, :create] do
+      resources :contract, only: [:index, :new, :create, :edit, :update ]
+    end
+  end
+  resources :pages, only: [:home, :about_us, :contact]
 end
---
+++ test/controllers/tenders_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class TendersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end

6ebfe18cb79da2ac6dd6e8f656b7a87ca16eda5e
Nirmaan Sonatun <nsonatun@hotmail.com>
2022-06-11 18:37:01 +0400

    Must work on bid view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :tenders
+  has_many :bids
+  has_many :contracts, through: :bids
   has_one_attached :avatar
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable

ab1c0928240c02ae8617f9839203d57a7dc9aa98
Nirmaan Sonatun <nsonatun@hotmail.com>
2022-06-14 21:31:38 +0400

    Created Services,projects and contact view and pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'services', to: 'pages#services'
+  get 'projects', to: 'pages#projects'
+  get 'contact', to: 'pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tenders do
     resources :bids do
       resources :contracs, only: [:index, :new, :create, :edit, :update ]
     end
   end
-  resources :pages, only: [:home, :about_us, :contact]
 end

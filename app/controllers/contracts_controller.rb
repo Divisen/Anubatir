921f9673391bee9a85ca00a51bcd01c09dc6cd77
Mubeen Bankur <mobee33@hotmail.com>
2022-06-14 12:10:12 +0400

    contracts

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,33 +1,31 @@
 class ContractsController < ApplicationController
 
-  # def index
-  #   @contracts = Contract.all
-  # end
+  def index
+    @contracts = Contract.all
+  end
 
-  # def new
-  #   @contract = Contract.new
-  # end
+  def show
+    @contract = Contract.find(params[:id])
+  end
 
-  # def create
-  #   @contract = Contract.new(contract_params)
-  #   @contract.bid = @bid
-  #   @contract.user = current_user
-  #   if @contract.save
-  #     redirect_to contract_path(@contract)
-  #   else
-  #     render 'contracts/show'
-  #   end
-  # end
+  def new
+    @contract = Contract.new
+  end
 
-  # def edit
-  # end
+  def create
+    @contract = Contract.new(contract_params)
+    @bid = Bid.find(params[:bid_id])
+    @contract.bid = @bid
+    if @contract.save
+      redirect_to contract_path(@contract)
+    else
+      render 'contracts/show'
+    end
+  end
 
-  # def update
-  # end
+  private
 
-  # private
-
-  # def contract_params
-  #   params.require(:contract).permit(:has_client_signed, :has_builder_signed, :completed, :images, :duration)
-  # end
+  def contract_params
+    params.require(:contract).permit(:has_client_signed, :has_builder_signed, :completed, :images, :duration)
+  end
 end

d742714f318d40a8548c928e9a1deee00de7c2ee
Mubeen Bankur <mobee33@hotmail.com>
2022-06-19 14:24:01 +0400

    adding the paths for tenders and bids

--- app/models/tender.rb
+++ app/models/tender.rb
@@ -2,5 +2,6 @@ class Tender < ApplicationRecord
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
   belongs_to :user
+  has_many :bids
   has_many_attached :images
 end

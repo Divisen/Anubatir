5619a1a73a3e5aeec745c7de0f70e2dc575b1173
Mubeen Bankur <mobee33@hotmail.com>
2022-06-23 13:55:54 +0400

    added validity such that user can quote on a bid once

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -3,5 +3,5 @@ class Bid < ApplicationRecord
   belongs_to :tender
   has_many :items, dependent: :destroy
   accepts_nested_attributes_for :items, reject_if: :all_blank, allow_destroy: true
-  # validates :user, uniqueness: { message: "should happen once per year" }, on: :create
+  validates :user, uniqueness: { scope: :tender }
 end

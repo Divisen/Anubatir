fe58e4b0400b7a8e6b774376aec974d48c260c3f
divisen rungadoo <divisen@engineer.com>
2022-06-28 21:03:42 +0400

    Conditions of platform to be completed

--- db/schema.rb
+++ db/schema.rb
@@ -116,7 +116,7 @@ ActiveRecord::Schema.define(version: 2022_06_28_132925) do
     t.string "first_name"
     t.string "last_name"
     t.string "username"
-    t.boolean "is_builder", default: false
+    t.boolean "is_builder"
     t.boolean "admin"
     t.string "company_name"
     t.string "address"

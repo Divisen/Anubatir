6a50e3600c5fb46875b52218371e1c1337e9fbe9
Mubeen Bankur <mobee33@hotmail.com>
2022-06-29 19:06:44 +0400

    working on chat

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_28_132925) do
+ActiveRecord::Schema.define(version: 2022_06_29_150536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,11 @@ ActiveRecord::Schema.define(version: 2022_06_28_132925) do
     t.index ["user_id"], name: "index_bids_on_user_id"
   end
 
+  create_table "chatrooms", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "contracts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -76,6 +81,16 @@ ActiveRecord::Schema.define(version: 2022_06_28_132925) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["bid_id"], name: "index_items_on_bid_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "tenders", force: :cascade do |t|
@@ -116,7 +131,7 @@ ActiveRecord::Schema.define(version: 2022_06_28_132925) do
     t.string "first_name"
     t.string "last_name"
     t.string "username"
-    t.boolean "is_builder"
+    t.boolean "is_builder", default: false
     t.boolean "admin"
     t.string "company_name"
     t.string "address"
@@ -131,6 +146,8 @@ ActiveRecord::Schema.define(version: 2022_06_28_132925) do
   add_foreign_key "bids", "users"
   add_foreign_key "contracts", "bids"
   add_foreign_key "items", "bids"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "tenders", "users"
   add_foreign_key "testimonials", "users"
 end

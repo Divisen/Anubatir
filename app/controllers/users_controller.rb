aa6dedf03bc6645339e201a4356092ce9b8443c2
Mubeen Bankur <mobee33@hotmail.com>
2022-06-30 10:23:41 +0400

    bugs fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @chatrooms = Chatroom.all
+  end
+
+  def click
+    @user = User.find(params[:id])
     @chatroom = Chatroom.new
     @chatroom_name = get_name(@user)
     @single_chatroom = Chatroom.where(name: @chatroom_name).first || Chatroom.create_private_chatroom([@user, current_user], @chatroom_name)
-    @message = Message.new
+    redirect_to chatroom_path(@single_chatroom)
   end
 
   private

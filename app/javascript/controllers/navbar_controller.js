34c5b47e5cc7ff612008b34016d8db470a1bc2e3
Nirmaan Sonatun <nsonatun@hotmail.com>
2022-07-02 13:17:26 +0400

    Done with type js

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   updateNavbar() {
-    if (window.scrollY >= window.innerHeight / 1.5) {
+    if (window.scrollY >= window.innerHeight / 1.2) {
       this.element.classList.add("navbar-lewagon-white")
     } else {
       this.element.classList.remove("navbar-lewagon-white")

5da81b608e8dec432378882c2b43b881d6ee864a
Mubeen Bankur <mobee33@hotmail.com>
2022-07-06 04:15:32 +0400

    video chat

--- app/models/video_chat.rb
+++ app/models/video_chat.rb
@@ -1,6 +1,12 @@
 class VideoChat < ApplicationRecord
   has_many :vloggers
   has_many :users, through: :vloggers
+  before_create :add_unique_name
+  def add_unique_name
+    unless self.unique_name.present?
+      self.unique_name = (0...15).map { ('a'..'z').to_a[rand(26)] }.join
+    end
+  end
 
   def self.create_private_video_chat(users, video_chat_name)
     single_video_chat = VideoChat.create(name: video_chat_name)

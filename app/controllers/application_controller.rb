15ef884dc4b736b5e0431f437bf5be2a4c8f12e1
Nirmaan Sonatun <nsonatun@hotmail.com>
2022-07-07 20:58:59 +0400

    Setting up meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :is_builder, :company_name, :address])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :grade_of_contractor, :avatar, :username])
   end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
